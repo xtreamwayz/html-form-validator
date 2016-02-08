5cf0e8dd8d010eda60dc896d85177c06d90bb5e0
Geert Eltink <xtreamwayz@gmail.com>
2016-02-08 19:49:02 +0100

    Input radio type should check for valid value

--- src/FormElement/Radio.php
+++ src/FormElement/Radio.php
@@ -3,6 +3,7 @@
 namespace Xtreamwayz\HTMLFormValidator\FormElement;
 
 use DOMElement;
+use DOMXPath;
 use Zend\InputFilter\InputInterface;
 use Zend\Validator;
 
@@ -14,5 +15,19 @@ class Radio extends AbstractFormElement
     protected function attachDefaultValidators(InputInterface $input, DOMElement $element)
     {
         // TODO: Errors should attach only to one radio element
+
+        $haystack = [];
+        $name = $element->getAttribute('name');
+        $xpath = new DOMXPath($this->document);
+
+        /** @var DOMElement $node */
+        foreach ($xpath->query('//input[@type="radio"][@name="' . $name . '"]') as $node) {
+            $haystack[] = $node->getAttribute('value');
+        }
+
+        $input->getValidatorChain()
+              ->attach(new Validator\InArray([
+                  'haystack' => $haystack,
+              ]));
     }
 }
