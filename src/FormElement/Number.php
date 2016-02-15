37475d4235e39074c02c267b29e45720aeeeedc6
Geert Eltink <xtreamwayz@gmail.com>
2016-02-15 11:11:15 +0100

    Oops... remove debug code :D

--- src/FormElement/Number.php
+++ src/FormElement/Number.php
@@ -20,23 +20,17 @@ class Number extends AbstractFormElement
     {
         $step = ($this->element->hasAttribute('step')) ? $this->element->getAttribute('step') : 1;
 
-        var_dump('step:' . $step .':'. (int) $step);
-
         if (is_numeric($step) && (int) $step == $step) {
-            var_dump(__LINE__);
             $this->attachValidatorByName('isint');
         } elseif (is_numeric($step) && (float) $step == $step) {
-            var_dump(__LINE__);
             $this->attachValidatorByName('isfloat', [
                 'locale' => 'en'
             ]);
         } elseif ($step != 'any') {
-            var_dump(__LINE__);
             throw new InvalidArgumentException('Number step must be an int, float or the text "any"');
         }
 
         if ($this->element->hasAttribute('min')) {
-            var_dump('min:' . $this->element->getAttribute('min'));
             $this->attachValidatorByName('greaterthan', [
                 'min'       => $this->element->getAttribute('min'),
                 'inclusive' => true,
@@ -44,7 +38,6 @@ class Number extends AbstractFormElement
         }
 
         if ($this->element->hasAttribute('max')) {
-            var_dump('max:' . $this->element->getAttribute('max'));
             $this->attachValidatorByName('lessthan', [
                 'max'       => $this->element->getAttribute('max'),
                 'inclusive' => true,
