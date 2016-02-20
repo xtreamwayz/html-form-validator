52eb4474c13ab3acce779afb2b8e25a7fb6fac6a
Geert Eltink <xtreamwayz@gmail.com>
2016-02-20 18:54:36 +0100

    #17 - Add support for select multiple attribute

--- src/FormElement/Select.php
+++ src/FormElement/Select.php
@@ -2,6 +2,7 @@
 
 namespace Xtreamwayz\HTMLFormValidator\FormElement;
 
+use Zend\Validator\Explode as ExplodeValidator;
 use Zend\Validator\InArray as InArrayValidator;
 
 class Select extends BaseFormElement
@@ -9,6 +10,35 @@ class Select extends BaseFormElement
     protected function getValidators()
     {
         $validators = [];
+
+        if ($this->node->hasAttribute('multiple')) {
+            $validators[] = [
+                'name'    => ExplodeValidator::class,
+                'options' => [
+                    'validator'      => $this->getInArrayValidator(),
+                    'valueDelimiter' => null, // skip explode if only one value
+                ],
+            ];
+        } else {
+            $validators[] = $this->getInArrayValidator();
+        }
+
+        return $validators;
+    }
+
+    private function getInArrayValidator()
+    {
+        return [
+            'name'    => InArrayValidator::class,
+            'options' => [
+                'haystack' => $this->getValueOptions(),
+                'strict'   => false,
+            ],
+        ];
+    }
+
+    private function getValueOptions()
+    {
         $haystack = [];
 
         /** @var \DOMElement $option */
@@ -16,13 +46,6 @@ class Select extends BaseFormElement
             $haystack[] = $option->getAttribute('value');
         }
 
-        $validators[] = [
-            'name'    => InArrayValidator::class,
-            'options' => [
-                'haystack' => $haystack,
-            ],
-        ];
-
-        return $validators;
+        return $haystack;
     }
 }
