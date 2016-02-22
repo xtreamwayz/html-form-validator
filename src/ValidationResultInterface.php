600b3f6e85390bd75ecd5d32bae2b66ce2497148
Geert Eltink <xtreamwayz@gmail.com>
2016-02-22 21:57:39 +0100

    #35 - Reduce boilerplate code

--- src/ValidationResultInterface.php
+++ src/ValidationResultInterface.php
@@ -7,21 +7,25 @@ interface ValidationResultInterface
     /**
      * ValidationResult constructor.
      *
-     * @param array $rawInputData
-     * @param array $validatedData
-     * @param array $errors
+     * @param array       $rawInputData
+     * @param array       $validatedData
+     * @param array       $errors
+     * @param null|string $method
      */
-    public function __construct(array $rawInputData, array $validatedData, array $errors);
+    public function __construct(array $rawInputData, array $validatedData, array $errors, $method = null);
 
     /**
      * Check if the validation was successful
+     *
+     * If there are no validation messages set and the request method is null or POST,
+     * the validation result object is considered valid.
      *
      * @return bool
      */
     public function isValid();
 
     /**
-     * Get error messages
+     * Get validation messages
      *
      * @return array
      */
