36f97c19f7d5cff6f8377be33b8270183af6451d
Geert Eltink <xtreamwayz@gmail.com>
2016-02-26 13:04:53 +0100

    Fix number validator

--- src/FormElement/Number.php
+++ src/FormElement/Number.php
@@ -2,11 +2,9 @@
 
 namespace Xtreamwayz\HTMLFormValidator\FormElement;
 
-use InvalidArgumentException;
-use Zend\I18n\Validator\IsFloat as IsFloatValidator;
-use Zend\I18n\Validator\IsInt as IsIntValidator;
 use Zend\Validator\GreaterThan as GreaterThanValidator;
 use Zend\Validator\LessThan as LessThanValidator;
+use Zend\Validator\Regex as RegexValidator;
 use Zend\Validator\Step as StepValidator;
 
 class Number extends BaseFormElement
@@ -19,24 +17,7 @@ class Number extends BaseFormElement
         // thousand separator. The prior use of the i18n Float validator
         // allowed the thousand separator, which resulted in wrong numbers
         // when casting to float.
-        //$validators[] = new RegexValidator('(^-?\d*(\.\d+)?$)');
-
-        $step = ($this->node->hasAttribute('step')) ? $this->node->getAttribute('step') : 1;
-
-        if (is_numeric($step) && (int) $step == $step) {
-            $validators[] = [
-                'name' => IsIntValidator::class,
-            ];
-        } elseif (is_numeric($step) && (float) $step == $step) {
-            $validators[] = [
-                'name'    => IsFloatValidator::class,
-                'options' => [
-                    'locale' => 'en',
-                ],
-            ];
-        } elseif ($step != 'any') {
-            throw new InvalidArgumentException('Number step must be an int, float or the text "any"');
-        }
+        $validators[] = new RegexValidator('(^-?\d*(\.\d+)?$)');
 
         if ($this->node->hasAttribute('min')) {
             $validators[] = [
@@ -65,7 +46,7 @@ class Number extends BaseFormElement
                 'name'    => StepValidator::class,
                 'options' => [
                     'baseValue' => $this->node->getAttribute('min') ?: 0,
-                    'step'      => $step,
+                    'step'      => $this->node->getAttribute('step') ?: 1,
                 ],
             ];
         }
