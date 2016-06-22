c26055e088f2eb9930e9b19a78e54ec3bcb1d06c
Geert Eltink <xtreamwayz@gmail.com>
2016-06-22 11:10:28 +0200

    Remove duplicate code in the password element

--- src/FormElement/Password.php
+++ src/FormElement/Password.php
@@ -11,42 +11,8 @@ declare(strict_types = 1);
 
 namespace Xtreamwayz\HTMLFormValidator\FormElement;
 
-use Zend\Filter\StripNewlines as StripNewlinesFilter;
-use Zend\Validator\Regex as RegexValidator;
-use Zend\Validator\StringLength as StringLengthValidator;
+use Xtreamwayz\HTMLFormValidator\FormElement\Text as TextElement;
 
-class Password extends BaseFormElement
+class Password extends TextElement
 {
-    protected function getFilters() : array
-    {
-        return [
-            ['name' => StripNewlinesFilter::class],
-        ];
-    }
-
-    protected function getValidators() : array
-    {
-        $validators = [];
-
-        if ($this->node->hasAttribute('minlength') || $this->node->hasAttribute('maxlength')) {
-            $validators[] = [
-                'name'    => StringLengthValidator::class,
-                'options' => [
-                    'min' => $this->node->getAttribute('minlength') ?: 0,
-                    'max' => $this->node->getAttribute('maxlength') ?: null,
-                ],
-            ];
-        }
-
-        if ($this->node->hasAttribute('pattern')) {
-            $validators[] = [
-                'name'    => RegexValidator::class,
-                'options' => [
-                    'pattern' => sprintf('/%s/', $this->node->getAttribute('pattern')),
-                ],
-            ];
-        }
-
-        return $validators;
-    }
 }
